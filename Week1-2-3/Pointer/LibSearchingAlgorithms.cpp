769ad72186c0af31d2186bf14fcf89b253d99e9d
thanhngan22 <nganbui9a2017@gmail.com>
2022-10-07 15:18:41 +0700

    :writing_hand: DSA/ Assignments/ Week 1-2-3/ Pointer/ review some basic function

---
+++ Week1-2-3/Pointer/LibSearchingAlgorithms.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// From No. 17. to No. 20. are Searching Algorithms. Return the first position found, else, return −1
+
+// 17. Sequential Search.
+int LinearSearch(int* a, int n, int key) {
+
+}
+
+// 18. Sequential Search uses flag.
+int SentinelLinearSearch(int* a, int n, int key) {
+
+}
+
+// 19. Binary Search in sorted array a.
+int BinarySearch(int* a, int n, int key){
+
+}
+
+// 20. Binary Search in sorted array a uses recursion.
+int RecursiveBinarySearch(int* a, int left, int right, int key){
+
+}
