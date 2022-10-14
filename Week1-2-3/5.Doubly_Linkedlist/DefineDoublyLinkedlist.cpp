08c4b74df6dd1581417c72008d54b15b8f1b362f
thanhngan22 <nganbui9a2017@gmail.com>
2022-10-14 18:26:50 +0700

    :blossom: finished all doubly linked list without testing, fix some logic bugs in define linked list file

--- Week1-2-3/5.Doubly_Linkedlist/DefineDoublyLinkedlist.cpp
+++ Week1-2-3/5.Doubly_Linkedlist/DefineDoublyLinkedlist.cpp
@@ -146,46 +146,167 @@ bool addPos(DList* &L, int data, int pos) {
     DNODE *pNode = createDNode(data);
     pNode->pNext = temp;
     pNode->pPrev = temp->pPrev;
-    temp->pPrev = pNode;
+    temp->pPrev->pNext = pNode;
     return true;
 }
 
 // 11. Remove an integer at a position of a given DList:
 void removePos(DList* &L, int pos) {
-
+    if (L->pHead == NULL) {
+        return;
+    }
+    int count = 0;
+    DNODE *pNode = L->pHead;
+    while (pNode != NULL && count != pos) {
+        count ++;
+        if (pNode->pNext == NULL) {
+            if (count == pos) {
+                // remove pNode 
+                pNode->pPrev->pNext = pNode->pNext;
+                pNode->pNext->pPrev = pNode->pPrev;
+                delete pNode;
+                return;
+            } else {
+                // not found pos in list
+                return;
+            }
+        }
+    }
+    pNode->pPrev->pNext = pNode->pNext;
+    pNode->pNext->pPrev = pNode->pPrev;
+    delete pNode;
 }
 
 // 12. Insert an integer before a value of a given DList:
 bool addBefore(DList* L, int data, int val) {
-
+    if (L->pHead == NULL) {
+        return false;
+    }
+    if (L->pHead->key == val) {
+        addHead(L, data);
+        return true;
+    }
+    DNODE *pNode = L->pHead;
+    while (pNode != NULL && pNode->key != val) {
+        pNode = pNode->pNext;
+    }
+    if (pNode == NULL) {
+        // val not found
+        return false;
+    }
+    DNODE *newDNode = createDNode(data);
+    pNode->pPrev->pNext = newDNode;
+    newDNode->pPrev = pNode->pPrev;
+    newDNode->pNext = pNode;
+    pNode->pPrev = newDNode;
+    return true;
 }
 
 // 13. Insert an integer after a value of a given DList:
 bool addAfter(DList* L, int data, int val) {
-
+    if (L->pHead == NULL) {
+        return false;
+    }
+    DNODE *temp = L->pHead;
+    while (temp != NULL && temp->key != val) {
+        temp = temp->pNext;
+    }
+    if (temp == NULL) {
+        return false;
+    }
+    DNODE *pNode = createDNode(data);
+    pNode->pPrev = temp;
+    pNode->pNext = temp->pNext;
+    temp->pNext->pPrev = pNode;
+    temp->pNext = pNode;
+    return true;
 }
 
 // 14. Print all elements of a given DList:
 void printList(DList* L) {
+    if (L->pHead == NULL) {
+        return;
+    }
+    DNODE *pNode = L->pHead;
+    while (pNode != NULL) {
+        cout << pNode->key << " ";
+        pNode = pNode->pNext;
+    }
 
 }
 
 // 15. Count the number of elements DList:
 int countElements(DList* L) {
-
+    if (L->pHead == NULL) {
+        return 0;
+    }
+    DNODE *pNode = L->pHead;
+    int count = 0;
+    while (pNode != NULL) {
+        count ++;
+        pNode = pNode->pNext;
+    }
+    return count ;
 }
 
-// 16. Create a new List by reverse a given DList:
+// 16. Create a new DList by reverse a given DList:
 DList* reverseList(DList* L) {
-
+    DList *L2 = createDList(createDNode(L->pHead->key));
+    DNODE *temp = L->pHead->pNext;
+    while (temp != NULL) {
+        addHead(L2, temp->key);
+        temp = temp->pNext;
+    }
+    return L2;
 }
 
 // 17. Remove all duplicates from a given DList:
 void removeDuplicate(DList* &L) {
-
+    if (L->pHead == NULL) {
+        return ;
+    }
+    DNODE *pNode = L->pHead;
+    while (pNode != NULL) {
+        DNODE *temp = pNode->pNext;
+        while (temp != NULL) {
+            if (temp->key == pNode->key) {
+                // remove temp
+                temp->pPrev->pNext = temp->pNext;
+                temp->pNext->pPrev = temp->pPrev;
+                DNODE *pNode = temp->pNext;
+                delete temp;
+                temp = pNode;
+            } else {
+                temp = temp->pNext;
+            }
+        }
+        pNode = pNode->pNext;
+    }
 }
 
 // 18. Remove all key value from a given DList:
 bool removeElement(DList* &L, int key) {
+    if (L->pHead == NULL) {
+        return false;
+    }
+    
+    // if pHead has key value
+    while (L->pHead->key == key) {
+        removeHead(L);
+    }
+    DNODE *temp = L->pHead;
+    while (temp != NULL) {
+        if (temp->key == key) {
+            // remove temp
+            DNODE *pNode = temp;
+            temp->pPrev->pNext = temp->pNext;
+            temp->pNext->pPrev = temp->pPrev;
+            temp = temp->pNext;
+            delete pNode;
+        } else {
+            temp = temp->pNext;
+        }
+    }
+    return true;
 
 }
