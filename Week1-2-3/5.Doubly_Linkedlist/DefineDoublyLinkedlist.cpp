d13d899279494ebd03509f0e747430d78da41e99
thanhngan22 <nganbui9a2017@gmail.com>
2022-10-14 12:01:30 +0700

    :blossom: finished 10 basic funcs in doubly linked list

--- Week1-2-3/5.Doubly_Linkedlist/DefineDoublyLinkedlist.cpp
+++ Week1-2-3/5.Doubly_Linkedlist/DefineDoublyLinkedlist.cpp
@@ -1,55 +1,191 @@
 #include "LibDoublyLinkedList.h"
 
-// 1. Initialize a NODE from a given integer:
-DNODE* createNode(int data);
+// 1. Initialize a DNODE from a given integer:
+DNODE* createDNode(int data) {
+    DNODE *pNode = new DNODE;
+    pNode->key = data;
+    pNode->pNext = NULL;
+    pNode->pPrev = NULL;
+    return pNode;
+}
 
-// 2. Initialize a List from a give NODE:
-DList* createList(DNODE* pNode);
+// 2. Initialize a List from a give DNODE:
+DList* createDList(DNODE* pNode) {
+    DList *pList = new DList;
+    pList->pHead = pNode;
+    pList->pTail = pNode;
+    pNode->pNext = NULL;
+    pNode->pPrev = NULL;
+    return pList;
+}
 
 // 3. Insert an integer to the head of a given List:
-bool addHead(DList* &L, int data);
+bool addHead(DList* &L, int data) {
+    DNODE *pNode = createDNode(data);
+    if (L->pHead == NULL) {
+        createDList(pNode);
+        return true;
+    }
+    pNode->pNext = L->pHead;
+    L->pHead = pNode;
+    return true;
+}
 
 // 4. Insert an integer to the tail of a given List:
-bool addTail(DList* &L, int data);
+bool addTail(DList* &L, int data) {
+    DNODE *pNode = createDNode(data);
+    if (L->pHead == NULL) {
+        createDList(pNode);
+        return true;
+    }
+    pNode->pPrev = L->pTail;
+    L->pTail = pNode;
+    return true;
 
-// 5. Remove the first NODE of a given List:
-void removeHead(DList* &L);
+}
 
-// 6. Remove the last NODE of a given List:
-void removeTail(DList* &L);
+// 5. Remove the first DNODE of a given List:
+void removeHead(DList* &L) {
+    if (L->pHead == NULL) {
+        return ;
+    }
+    DNODE *pNode = L->pHead;
+    L->pHead = L->pHead->pNext;
+    L->pHead->pPrev = NULL;
+    delete pNode;
+}
 
-// 7. Remove all NODE from a given List:
-void removeAll(DList* &L);
+// 6. Remove the last DNODE of a given List:
+void removeTail(DList* &L) {
+    if (L->pHead == NULL ) {
+        return;
+    }
+    DNODE *pNode = L->pTail;
+    L->pTail = L->pTail->pPrev;
+    delete pNode;
+}
 
-// 8. Remove node before the node has val value in a given List:
-void removeBefore(DList* L, int val);
+// 7. Remove all DNODE from a given List:
+void removeAll(DList* &L) {
+    if (L->pHead == NULL) {
+        return;
+    }
+    DNODE *pNode = L->pHead;
+    while (L->pHead != NULL && L->pTail != NULL) {
+        L->pHead = L->pHead->pNext;
+        if (pNode == L->pTail) {
+            L->pTail = NULL;
+        }
+        delete pNode;
+    }
+}
+
+// 8. Remove node before the node has val value in a given DList:
+void removeBefore(DList* L, int val) {
+    DNODE *pNode = L->pHead;
+    while (pNode != NULL && pNode->key != val) {
+        pNode = pNode->pNext;
+    }
+    if (pNode == NULL) {
+        // val not found
+        return;
+    } else {
+        DNODE *temp = pNode->pPrev;
+        temp->pPrev->pNext = pNode;
+        pNode->pPrev = temp->pPrev;
+        delete temp;
+    }
+}
 
 // 9. Remove node after the node has val value in a given List:
-void removeAfter(DList* L, int val);
+void removeAfter(DList* L, int val) {
+    if (L->pHead == NULL) {
+        return;
+    }
+    DNODE *pNode = L->pHead;
+    while (pNode != NULL && pNode->key != val) {
+        pNode = pNode->pNext;
+    }
+    if (pNode == NULL) {
+        // val not found
+        return ;
+    } else {
+        DNODE *temp = pNode->pNext;
+        pNode->pNext = temp->pNext;
+        temp->pNext->pPrev = pNode;
+        delete temp;
+    }
+}
 
-// 10. Insert an integer at a position of a given List:
-bool addPos(DList* &L, int data, int pos);
+// 10. Insert an integer at a position of a given DList:
+bool addPos(DList* &L, int data, int pos) {
+    if (L->pHead == NULL) {
+        if (pos == 0) {
+            DNODE *pNode = createDNode(data);
+            L = createDList(pNode);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    int count = 0;
+    DNODE *temp = L->pHead;
+    while (temp != NULL && count != pos) {
+        count ++;
+        if (temp->pNext == NULL) {
+            if (count == pos) {
+                // insert to the tail
+                addTail(L, data);
+                return true;
+            } else {
+                return false;
+            }
+        } 
+        temp = temp->pNext;
+    }
+    DNODE *pNode = createDNode(data);
+    pNode->pNext = temp;
+    pNode->pPrev = temp->pPrev;
+    temp->pPrev = pNode;
+    return true;
+}
 
-// 11. Remove an integer at a position of a given List:
-void removePos(DList* &L, int pos);
+// 11. Remove an integer at a position of a given DList:
+void removePos(DList* &L, int pos) {
 
-// 12. Insert an integer before a value of a given List:
-bool addBefore(DList* L, int data, int val);
+}
 
-// 13. Insert an integer after a value of a given List:
-bool addAfter(DList* L, int data, int val);
+// 12. Insert an integer before a value of a given DList:
+bool addBefore(DList* L, int data, int val) {
 
-// 14. Print all elements of a given List:
-void printList(DList* L);
+}
 
-// 15. Count the number of elements List:
-int countElements(DList* L);
+// 13. Insert an integer after a value of a given DList:
+bool addAfter(DList* L, int data, int val) {
 
-// 16. Create a new List by reverse a given List:
-DList* reverseList(DList* L);
+}
 
-// 17. Remove all duplicates from a given List:
-void removeDuplicate(DList* &L);
+// 14. Print all elements of a given DList:
+void printList(DList* L) {
 
-// 18. Remove all key value from a given List:
-bool removeElement(DList* &L, int key);
+}
+
+// 15. Count the number of elements DList:
+int countElements(DList* L) {
+
+}
+
+// 16. Create a new List by reverse a given DList:
+DList* reverseList(DList* L) {
+
+}
+
+// 17. Remove all duplicates from a given DList:
+void removeDuplicate(DList* &L) {
+
+}
+
+// 18. Remove all key value from a given DList:
+bool removeElement(DList* &L, int key) {
+
+}
--- Week1-2-3/5.Doubly_Linkedlist/LibDoublyLinkedList.h
+++ Week1-2-3/5.Doubly_Linkedlist/LibDoublyLinkedList.h
@@ -13,56 +13,56 @@ struct DList {
     DNODE* pTail;
 };
 
-// 1. Initialize a NODE from a given integer:
-DNODE* createNode(int data);
+// 1. Initialize a DNODE from a given integer:
+DNODE* createDNode(int data);
 
-// 2. Initialize a List from a give NODE:
-DList* createList(DNODE* pNode);
+// 2. Initialize a DList from a give DNODE:
+DList* createDList(DNODE* pNode);
 
-// 3. Insert an integer to the head of a given List:
+// 3. Insert an integer to the head of a given DList:
 bool addHead(DList* &L, int data);
 
-// 4. Insert an integer to the tail of a given List:
+// 4. Insert an integer to the tail of a given DList:
 bool addTail(DList* &L, int data);
 
-// 5. Remove the first NODE of a given List:
+// 5. Remove the first DNODE of a given DList:
 void removeHead(DList* &L);
 
-// 6. Remove the last NODE of a given List:
+// 6. Remove the last DNODE of a given DList:
 void removeTail(DList* &L);
 
-// 7. Remove all NODE from a given List:
+// 7. Remove all DNODE from a given DList:
 void removeAll(DList* &L);
 
-// 8. Remove node before the node has val value in a given List:
+// 8. Remove node before the node has val value in a given DList:
 void removeBefore(DList* L, int val);
 
-// 9. Remove node after the node has val value in a given List:
+// 9. Remove node after the node has val value in a given DList:
 void removeAfter(DList* L, int val);
 
-// 10. Insert an integer at a position of a given List:
+// 10. Insert an integer at a position of a given DList:
 bool addPos(DList* &L, int data, int pos);
 
-// 11. Remove an integer at a position of a given List:
+// 11. Remove an integer at a position of a given DList:
 void removePos(DList* &L, int pos);
 
-// 12. Insert an integer before a value of a given List:
+// 12. Insert an integer before a value of a given DList:
 bool addBefore(DList* L, int data, int val);
 
-// 13. Insert an integer after a value of a given List:
+// 13. Insert an integer after a value of a given DList:
 bool addAfter(DList* L, int data, int val);
 
-// 14. Print all elements of a given List:
+// 14. Print all elements of a given DList:
 void printList(DList* L);
 
-// 15. Count the number of elements List:
+// 15. Count the number of elements DList:
 int countElements(DList* L);
 
-// 16. Create a new List by reverse a given List:
+// 16. Create a new List by reverse a given DList:
 DList* reverseList(DList* L);
 
-// 17. Remove all duplicates from a given List:
+// 17. Remove all duplicates from a given DList:
 void removeDuplicate(DList* &L);
 
-// 18. Remove all key value from a given List:
+// 18. Remove all key value from a given DList:
 bool removeElement(DList* &L, int key);
