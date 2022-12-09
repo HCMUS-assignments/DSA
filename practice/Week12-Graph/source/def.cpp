8740f0668c0c330a1463a03703b38855f4ff6a7e
thanhngan22 <nganbui9a2017@gmail.com>
2022-12-09 17:58:54 +0700

    :anchor: setup lab 6 - graph

---
+++ practice/Week12-Graph/source/def.cpp
@@ -0,0 +1,57 @@
+#include "lib.h"
+
+
+// Đọc ma trận kề từ file
+
+// Đọc danh sách kề từ file
+
+// --------------------------------------------------------------
+
+// I. Implement functions to provide information about a given graph
+
+// 1. check a graph is directed or undirected
+bool isDirected (int **a, int n);
+
+// 2. cal the number of edges and number of vertices
+int numEdges (int **a, int n);
+int numVertices (int **a, int n);
+
+// 3. degree of each vertices for undirected graph. In-degree and out-degree for directed graph
+
+
+// 4. list of isolated vertices/ leaf vertices
+
+// 5. is the given graph special: complete graph, circular graph, bigraph, complete bi-graph
+
+// 6. the number of connected components. how many of them are trees
+
+// 7. the number of cut vertices and bridge edges
+
+
+// -------------------------------------------------------
+
+// 4. generate a base undirected graph from a given directed graph
+
+// 5. generate a complement graph from a given undirected graph, outputting the corresponding adjacency matrix
+
+// 6.  Generate aConverse graph from a given directed graph, outputting the corresponding adjacency matrix.
+
+// 7. determined euler cycle from a given graph using Hiehozer's algorithm
+
+// 8.1. find the spanning tree of a given graph using DFS traversal
+
+// 8.2. find the spanning tree of a given graph using BFS traversal
+
+
+// 9.1. find the minimum spanning tree of a given graph using Prim algorithm
+
+// 9.2. find the minimum spanning tree of a given graph using Kruskal algorithm
+
+// 10. verify the connection between 2 vertices of a given graph
+
+// 11.1. find the shortest path between 2 vertices of a graph using Dijkstra algorithm
+
+// 11.2. find the shortest path between 2 vertices of a graph using Bellman-Ford algorithm
+
+// 11.3. find the shortest path between 2 vertices of a graph using Floyd-Warshall algorithm
+
